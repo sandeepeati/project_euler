c275d4dba38495bf0ef2f6ca86ada500f1f37b0a
sandeep eti <sandeep.eati1106@gmail.com>
2018-02-08 14:03:30 +0530

    Add files via upload

--- p7.py
+++ p7.py
@@ -1,4 +1,11 @@
-# primes is a list with 2 already added
+#
+#
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+#  we can see that the 6th prime is 13.
+#
+# What is the 10 001st prime number?
+
+
 primes = [2]
 
 
@@ -10,14 +17,18 @@ def is_prime(num, primes_list):
     for prime in primes_list:
         if num % prime == 0:
             return False
-        return True
+    return True
 
 
 # generating primes
 # the idea is that every non-prime number is a multiple of any of the primes below it
-def primes_gen(n):
-    yield 2
-    for i in range(3, n + 1, 2):
+def primes_gen():
+    for i in range(3, 100000000000000, 2):
         if is_prime(i, primes):
             primes.append(i)
-            yield i
+            if len(primes) == 10001:
+                break
+
+
+primes_gen()
+print(primes[-1])
